74f51ead82d703d55b28647dcd84778ce337d456
Pleshko Pavel <pavelpleshko92@mail.ru>
2018-08-06 10:33:33 +0300

    master

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ var nodemailer = require("nodemailer");
 var bodyParser = require('body-parser');
 var compression = require('compression');
 var credentials = require('./environment/environment').mailCredentials;
-var cors = require('cors')
+var cors = require('cors');
+var helmet = require('helmet');
 var assisters = require('./helpers/lib');
 var app=express();
 var PORT = 8080;
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
 app.use(cors());
+app.use(helmet());
 app.use(compression());
 
 var smtpTransport = nodemailer.createTransport({
@@ -17,22 +19,21 @@ var smtpTransport = nodemailer.createTransport({
     host: "smtp.yandex.ru",
     port:465,
     auth: {
-        user: credentials.email,
-        pass: credentials.pass
+        user: process.env.CLIENT_EMAIL,
+        pass: process.env.CLIENT_PASSWORD
     }
 });
 var oneYear = 1 * 365 * 24 * 60 * 60 *1000;
 app.use('/',express.static(__dirname + '/public',{maxAge:oneYear}));
-app.get('/',(req, res) => {  
+app.get('/*',(req, res) => {  
   res.sendFile(__dirname+'/public/index.html');
 });
 
 
 
 app.post('/send',function(req,res){
-	console.log(req.body);
 var mailOpts = {
-	to:credentials.to,
+	to:process.env.EMAIL_TO,
 	subject:req.body.name + " left a message from portfolio website",
 	from:credentials.email,
 	text:assisters.createMsg(req.body),
@@ -53,7 +54,7 @@ smtpTransport.sendMail(mailOpts,function(response,error){
 
 
 app.listen(PORT,function(){
-console.log("Express Started on Port "+PORT);
+console.log("Server started on Port "+PORT);
 });
 
 
